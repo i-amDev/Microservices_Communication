d3c9cddaed7ba85400e36bb5b11a0120459c88b8
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-11-24 13:41:56 +0530

    - Used "RestTemplate" to communicate between two microservices.

---
+++ consumer-service/src/main/java/com/learning/consumer_service/controller/RestTemplateController.java
@@ -0,0 +1,22 @@
+package com.learning.consumer_service.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/consumer")
+@RequiredArgsConstructor
+public class RestTemplateController {
+
+    private final RestTemplate restTemplate;
+
+    @GetMapping("/getInstance")
+    public String getInstance() {
+        String response = restTemplate.getForObject("http://localhost:8081/instance", String.class);
+        return response;
+    }
+
+}
---
+++ producer-service/src/main/java/com/learning/producer_service/controller/InstanceController.java
@@ -0,0 +1,23 @@
+package com.learning.producer_service.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+public class InstanceController {
+
+    @Value("${server.port}")
+    private String port;
+
+    private final String instanceID = UUID.randomUUID().toString();
+
+    @GetMapping("/instance")
+    public String getInstanceInfo() {
+        System.out.println("Producer called!!");
+        return "Request received at instance running on port : " + port + " with Instance ID as " + instanceID;
+    }
+
+}
