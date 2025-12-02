6b6f7663eb91bd2849a7bab2d65a31cbf4a75f8c
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-12-02 11:21:44 +0530

    - Added the necessary changes required to perform inter-service communication using the name of microservice using "RestClient".

--- consumer-service/src/main/java/com/learning/consumer_service/restClient/RestClientConfig.java
+++ consumer-service/src/main/java/com/learning/consumer_service/restClient/RestClientConfig.java
@@ -1,5 +1,6 @@
 package com.learning.consumer_service.restClient;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -8,8 +9,14 @@ import org.springframework.web.client.RestClient;
 public class RestClientConfig {
 
     @Bean
-    public RestClient restClient() {
-        return RestClient.create();
+    @LoadBalanced
+    public RestClient.Builder loadBalancedRestClientBuilder() {
+        return RestClient.builder();
+    }
+
+    @Bean
+    public RestClient restClient(RestClient.Builder builder) {
+        return builder.baseUrl("http://producer-service").build();
     }
 
 }
--- consumer-service/src/main/java/com/learning/consumer_service/restClient/RestClientController.java
+++ consumer-service/src/main/java/com/learning/consumer_service/restClient/RestClientController.java
@@ -1,25 +1,32 @@
 package com.learning.consumer_service.restClient;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestClient;
 
 @RestController
 @RequestMapping("/consumer/restClient")
 @RequiredArgsConstructor
 public class RestClientController {
 
-    private final RestClient restClient;
+    @LoadBalanced
+    private final RestClientProvider restClientProvider;
+
+//    @GetMapping("/getInstance")
+//    public String getInstance() {
+//        String response = restClient.get()
+//                .uri("http://localhost:8081/instance")
+//                .retrieve()
+//                .body(String.class);
+//
+//        return response;
+//    }
 
     @GetMapping("/getInstance")
     public String getInstance() {
-        String response = restClient.get()
-                .uri("http://localhost:8081/instance")
-                .retrieve()
-                .body(String.class);
-
-        return response;
+        return restClientProvider.getInstanceInfo();
     }
+
 }
