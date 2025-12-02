b13e091a2e2286e02afaa615148ce8db14cd448f
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-12-02 11:19:41 +0530

    - Used the microservice name to perform inter-service communication using "feign" client.

--- consumer-service/src/main/java/com/learning/consumer_service/openFeign/OpenFeignClient.java
+++ consumer-service/src/main/java/com/learning/consumer_service/openFeign/OpenFeignClient.java
@@ -3,7 +3,8 @@ package com.learning.consumer_service.openFeign;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "producer-service", url = "http://localhost:8081")
+//@FeignClient(name = "producer-service", url = "http://localhost:8081")
+@FeignClient(name = "producer-service")
 public interface OpenFeignClient {
 
     @GetMapping("/instance")
