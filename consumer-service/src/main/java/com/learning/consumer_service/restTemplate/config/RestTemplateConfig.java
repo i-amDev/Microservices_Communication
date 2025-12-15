e4bd777830b9d438430cfab7c5bae25121cb2322
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-12-15 14:10:23 +0530

    - Refactored. - Removed unused imports.

--- consumer-service/src/main/java/com/learning/consumer_service/restTemplate/config/RestTemplateConfig.java
+++ consumer-service/src/main/java/com/learning/consumer_service/restTemplate/config/RestTemplateConfig.java
@@ -1,6 +1,5 @@
 package com.learning.consumer_service.restTemplate.config;
 
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
